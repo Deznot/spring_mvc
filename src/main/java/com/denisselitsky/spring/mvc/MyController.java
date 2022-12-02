dffca8b146e9af48f989175ebc2758ac3801c36c
Denis <denis.selitsky@gmail.com>
2022-12-02 13:38:53 +0300

    feat: add MyController

---
+++ src/main/java/com/denisselitsky/spring/mvc/MyController.java
@@ -0,0 +1,12 @@
+package com.denisselitsky.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+    @RequestMapping("/")
+    public String showMyView() {
+        return "myView";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/myView.jsp
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <div style="background-color: aquamarine">
+            <h2>Some text for view</h2>
+        </div>
+    </body>
+</html>
